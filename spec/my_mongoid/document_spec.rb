bef8e05f3522c567566bd0b1216b79fc4ee9972b
caok <caok1231@gmail.com>
2014-03-04 21:35:28 +0800

    add travis

--- spec/my_mongoid/document_spec.rb
+++ spec/my_mongoid/document_spec.rb
@@ -2,4 +2,29 @@ describe MyMongoid::Document do
   it "is a module" do
     expect(MyMongoid::Document).to be_a(Module)
   end
+
+  describe "classMethods: " do
+    it "is a module" do
+      expect(MyMongoid::Document).to be_a(Module)
+    end
+
+    it "is a mongoid model" do
+      expect(Event.is_mongoid_model?).to eq(true)
+    end
+
+    it "maintain a list of models" do
+      expect(MyMongoid.models).to include(Event)
+    end
+
+    it "maintains a map fields objects" do
+      expect(Event.fields).to be_a(Hash)
+      expect(Event.fields.keys).to include(*%w(public created_at))
+    end
+  end
+
+  describe ".new"
+  describe "#read_attribute"
+  describe "#write_attribute"
+  describe "#process_attributes"
+  describe "#new_record?"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,10 +7,10 @@ RSpec.configure do |config|
   MODELS = File.join(File.dirname(__FILE__), "app/models")
   $LOAD_PATH.unshift(MODELS)
 
-  #Dir[ File.join(MODELS, "*.rb") ].sort.each do |file|
-    #name = File.basename(file, ".rb")
-    #autoload name.camelize.to_sym, name
-  #end
+  Dir[ File.join(MODELS, "*.rb") ].sort.each do |file|
+    name = File.basename(file, ".rb")
+    autoload name.capitalize.to_sym, name
+  end
 
   # Drop all collections and clear the identity map before each spec.
   #config.before(:each) do
