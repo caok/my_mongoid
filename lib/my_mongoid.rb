9a6c5254f3ff2b2e0c5e5d859f7dfea3c487a1f8
caok <caok1231@gmail.com>
2014-03-06 08:09:38 +0800

    Implement Configuration

--- lib/my_mongoid.rb
+++ lib/my_mongoid.rb
@@ -2,6 +2,7 @@ require "my_mongoid/version"
 require "my_mongoid/document"
 require "my_mongoid/field"
 require "my_mongoid/duplicate_field_error"
+require "my_mongoid/configuration"
 
 module MyMongoid
   def self.models
@@ -10,5 +11,13 @@ module MyMongoid
 
   def self.register_model(model)
     models.push(model) unless models.include?(model)
+  end
+
+  def self.configuration
+    MyMongoid::Configuration.instance
+  end
+
+  def self.configure
+    yield(self.configuration)
   end
 end
---
+++ lib/my_mongoid/configuration.rb
@@ -0,0 +1,8 @@
+require 'singleton'
+
+module MyMongoid
+  class Configuration
+    include Singleton
+    attr_accessor :host, :database
+  end
+end
